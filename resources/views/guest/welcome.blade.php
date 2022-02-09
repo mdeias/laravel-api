c0163f7a4a56aaba8064faafafd5dd074db8affd
mdeias <mdeias@icloud.com>
2022-02-09 19:43:17 +0100

    separazione js e css admin/guest

--- resources/views/guest/welcome.blade.php
+++
@@ -1,9 +0,0 @@
-
-@extends('layouts.app') 
-    @section('content')
-
-         <div class="flex-center position-ref full-height">
-            <h1>PAGINA BENVENUTO</h1>
-        </div>
-    
-    @endsection
